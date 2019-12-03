81fc741fd2d3216b41779576bc90dc08eb5350bb
Bhavin Ranpara <bhavin.ranpara@emxcelsolutions.com>
2019-12-03 18:49:30 +0530

    Add label file class

---
+++ RWPickFlavor/Models/LabelView.swift
@@ -0,0 +1,21 @@
+//
+//  LabelView.swift
+//  RWPickFlavor
+//
+//  Created by Emxcel Mac on 03/12/19.
+//  Copyright © 2019 Emxcel Mac. All rights reserved.
+//
+
+import UIKit
+
+class LabelView: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
