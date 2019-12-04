aaac2c816aa094ad5ad3f0803d1813b0b2e8dca9
Bhavin Ranpara <bhavin.ranpara@emxcelsolutions.com>
2019-12-04 12:26:47 +0530

    commit file changes

--- RWPickFlavor/Models/LabelView.swift
+++ RWPickFlavor/Models/LabelView.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class LabelView: UILabel {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .blue
+    }
 
 }
